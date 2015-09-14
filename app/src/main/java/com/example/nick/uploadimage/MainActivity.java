dd6aba71747fbdd51e6e6c02a7af835f276afc6d
Nickolay <Golub.Nick@gmail.com>
2015-09-14 21:52:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nick/uploadimage/MainActivity.java
@@ -0,0 +1,225 @@
+package com.example.nick.uploadimage;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    Button choose, upload, download;
+    EditText editText;
+    ImageView imageView;
+    private String imagepath=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        choose = (Button)findViewById(R.id.choose);
+        upload = (Button)findViewById(R.id.upload);
+        download = (Button)findViewById(R.id.download);
+        imageView = (ImageView)findViewById(R.id.imageview);
+        editText = (EditText)findViewById(R.id.edittext);
+        choose.setOnClickListener(this);
+        upload.setOnClickListener(this);
+        download.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.choose:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), 1);
+                break;
+            case R.id.upload:
+                new Upload() {
+
+                    @Override
+                    public void getResult(String result) {
+                        Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+                    }
+                }.execute(imagepath);
+                break;
+            case R.id.download:
+                new Thread(new Runnable() {
+                    public void run() {
+                        downloadFile();
+                    }
+                }).start();
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            Uri selectedImageUri = data.getData();
+            imagepath = getPath(selectedImageUri);
+//            Bitmap bitmap= BitmapFactory.decodeFile(imagepath);
+//            imageview.setImageBitmap(bitmap);
+//            messageText.setText("Uploading file path:" +imagepath);
+
+        }
+    }
+
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+    public abstract class Upload extends AsyncTask<String, Void, String>{
+        String postReceiverUrl = "http://10.10.10.13:8080/upload";
+        String responseStr = null;
+
+        @Override
+        protected String doInBackground(String[] params) {
+            try {
+                String textFile = params[0];
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(postReceiverUrl);
+                File file = new File(textFile);
+                FileBody fileBody = new FileBody(file);
+                StringBody comment =  new StringBody(editText.getText().toString());
+                MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+                reqEntity.addPart("file", fileBody);
+                reqEntity.addPart("name", comment);
+                httpPost.setEntity(reqEntity);
+                HttpResponse response = httpClient.execute(httpPost);
+                HttpEntity resEntity = response.getEntity();
+                if (resEntity != null) {
+                    responseStr = EntityUtils.toString(resEntity).trim();
+                    System.out.println(responseStr);
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return responseStr;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            getResult(result);
+        }
+        public abstract void getResult(String result);
+    }
+
+    void downloadFile(){
+
+        try {
+            URL url = new URL("http://10.10.10.16:8080/files/df");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoOutput(true);
+
+            //connect
+            urlConnection.connect();
+
+            //set the path where we want to save the file
+            File SDCardRoot = Environment.getExternalStorageDirectory();
+            //create a new file, to save the <span id="IL_AD2" class="IL_AD">downloaded</span> file
+            File file = new File(SDCardRoot,"downloaded_file.png");
+
+            FileOutputStream fileOutput = new FileOutputStream(file);
+
+            //Stream used for reading the data from the internet
+            InputStream inputStream = urlConnection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageView.setImageBitmap(bitmap);
+            //close the output stream when complete //
+            fileOutput.close();
+
+
+        } catch (final MalformedURLException e) {
+            showError("Error : MalformedURLException " + e);
+            e.printStackTrace();
+        } catch (final IOException e) {
+            showError("Error : IOException " + e);
+            e.printStackTrace();
+        }
+        catch (final Exception e) {
+            showError("Error : Please check your internet connection " + e);
+        }
+    }
+    void showError(final String err){
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, err, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
